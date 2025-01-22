6ab282bd4d105f298c2a15b153ac0f378d6c2374
Zeeshan Khalid <khalidzeeshan783@gmail.com>
2025-01-21 23:40:29 -0800

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',  // This should match the name of your GitHub repository (case-sensitive)
-});
